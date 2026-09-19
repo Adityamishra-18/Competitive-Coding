8d2560f2aa0bd0a1a5a49264b9849f986917fca2
Adityamishra-18 <madityamishra05@gmail.com>
2026-09-19 19:59:16 +0530

    Add GFG solution: Union of Two Linked Lists✅✅

--- GeeksForGeeks/LinkedList/union-of-two-linked-lists.java
+++ GeeksForGeeks/LinkedList/union-of-two-linked-lists.java
@@ -1,2 +1,25 @@
-import java.util.HashSet;
-import java.util.Set;
+function merge(List a, List b)  
+{
+      if( list a is null ) return list b
+      if(list b is null ) return list a
+
+       List ans = null
+
+      if(a.data<b.data) // add a node to answer
+      {
+          ans=a
+          ans.next=merge(a.next,b)
+      }
+      else if(b.data<a.data) // add b node to answer
+      {
+          ans=b
+          ans.next=merge(a,b.next)
+      }
+      else     //This is important to handle duplicate elements
+      {
+          ans=b // you can write a or b(any)
+          ans.next=merge(a.next,b.next)  //move both pointers.
+      }
+
+               return ans
+}

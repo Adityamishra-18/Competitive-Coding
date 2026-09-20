115c136b64c25e3e850ce8a0d5be37924f048bbd
Adityamishra-18 <madityamishra05@gmail.com>
2026-09-20 12:06:02 +0530

    Add GFG solution: Common Elements | Practice

---
+++ GeeksForGeeks/Arrays/common-elements-practice.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public static ArrayList<Integer> commonElements(int a[], int b[]) {
+        // code here
+        ArrayList<Integer> result=new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int i=0;
+        int j=0;
+        while(i<a.length &&j<b.length){
+            if(a[i]==b[j]){
+                result.add(a[i]);
+                i++;
+                j++;
+            }else if(a[i]<b[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return result;
+    }
+}

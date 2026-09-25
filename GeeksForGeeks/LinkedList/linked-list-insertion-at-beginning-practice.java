b4c2ef39dc75690536c56b1acff070d34f0716d9
Adityamishra-18 <madityamishra05@gmail.com>
2026-09-25 14:19:33 +0530

    Add GFG solution: Linked List Insertion At Beginning | Practice

---
+++ GeeksForGeeks/LinkedList/linked-list-insertion-at-beginning-practice.java
@@ -0,0 +1,24 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node insertAtFront(Node head, int x) {
+        // code here
+        if(head==null){
+            return new Node(x);
+        }
+        Node newNode=new Node(x);
+        newNode.next=head;
+        return newNode;
+
+    }
+}
